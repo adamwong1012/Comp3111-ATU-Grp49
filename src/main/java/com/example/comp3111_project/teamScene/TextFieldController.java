50ab906d19e86014dad67a2df3b56ebd1856a235
jason218 <jw122322@gmail.com>
2022-11-17 20:12:21 +0800

    fix UI

--- src/main/java/com/example/comp3111_project/teamScene/TextFieldController.java
+++ src/main/java/com/example/comp3111_project/teamScene/TextFieldController.java
@@ -1,5 +1,7 @@
 package com.example.comp3111_project.teamScene;
 
+import com.example.comp3111_project.outputScene.OutputController;
+import com.example.comp3111_project.outputScene.OutputScene;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
@@ -7,7 +9,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,14 +37,18 @@ public class TextFieldController   {
 
 
     @FXML
-    void search(ActionEvent event) {
+    void search(ActionEvent event) throws IOException {
         inputResult = inputField.getCharacters().toString();
-        System.out.println(inputResult);
+        Stage outputStage = new Stage();
+        OutputScene outputScene = new OutputScene();
+        outputScene.getController().setSearch(inputResult);
+        outputStage.setScene(outputScene);
+        outputStage.show();
     }
 
     @FXML
     void exit(ActionEvent event) {
-
+        this.exitButton.fireEvent(event);
     }
 
     public String getInputResult() {
